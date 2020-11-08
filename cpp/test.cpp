578823dbf8ddda25e4e30048b372d52c04443d5b
smuzoey <smuzoey@gmail.com>
2020-11-08 14:51:59 +0800

    Enter

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,30 +1,16 @@
-#include<cstdio>
-#include<cmath>
-#include<algorithm>
-#include<cstring>
+#include<bits/stdc++.h>
 using namespace std;
 
-int mp[5000][5000];
+class Solution {
+	public:
+    int minimumEffortPath(vector<vector<int> >& heights) {
+    	return 0;
+	}
+
+};
 
 int main() {
 
-	int n;
-	scanf("%d", &n);
-	for(int i = 0; i < n; i++) {
-		for(int j = 0; j < n; j++) {
-			scanf("%d", &mp[i][j]);
-			getchar();
-		}
-	}
-
-
-	for(int i = 0; i < n; i++) {
-		for(int j = n-1; j >= 0; j--) {
-			if(i == n-1 && j == 0) printf("%d\n", mp[j][i]);
-			else printf("%d,", mp[j][i]);
-		}
-
-	}
-
+	cout << "***" << endl;
 	return 0;
 }
